99446620da3f606d4d97d98c00a9073a5e1a93fb
rezaulraj <rezaul.coderpro@gmail.com>
2025-09-20 01:30:21 +0600

    update cors file commit

--- src/utils/cors.js
+++ src/utils/cors.js
@@ -1,10 +1,22 @@
 import cors from "cors";
 
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  "http://localhost:3000",
+  "http://localhost:5173",
+];
+
 const corsOptions = {
-  origin: process.env.CLIENT_URL || "http://localhost:3000",
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 export default cors(corsOptions);
